eb6751cfaeff44816e1fc6da8b893aa55bd8a89e
Raffaele P. Guidi <raffaele.p.guidi@gmail.com>
2015-05-26 16:26:09 +0200

    added task complete

--- bpm-application.js
+++ bpm-application.js
@@ -27,6 +27,15 @@ Bpm = {
               Session.set('formData', res);
             }
         });
+    },
+    complete: function(taskId, properties) {
+        Meteor.call("complete", taskId, properties, function(err, res) {
+            if (err) {
+              console.log("errore: %s" , err.message);
+            } else {
+                Session.set('formData', undefined);
+            }
+        });
     }
 }
 
@@ -48,6 +57,10 @@ if (Meteor.isClient) {
     formData: function() {
         return Session.get('formData');
     },
+    taskSelected: function() {
+        console.log("eccomi %s, %s", JSON.stringify(Session.get('formData')), Session.get('formData') != null && Session.get('formData') != undefined);
+        return Session.get('formData') != null && Session.get('formData') != undefined;
+    },
     pages: function() {
         return Session.get("pages");
     },
@@ -57,23 +70,49 @@ if (Meteor.isClient) {
   });
 
   Template.hello.events({
-    'click button': function (evt) {
+    'click .refresh': function (evt) {
+        console.log('refresh');
         Bpm.refreshTaskList();
+        Session.set("formData", undefined);
+        return false;
     },
     'click .claim': function () {
+        console.log('claim');
         Bpm.formData(this.id);
+        return false;
+    },
+    'click .unselect': function () {
+        console.log('unselect');
+        Session.set('formData', undefined);
+        return false;
     },
     'click .page': function (evt) {
+        console.log('page');
         Bpm.start = ($(evt.target).attr('data-page')-1) * Bpm.size;
         Session.set('currentPage', $(evt.target).attr('data-page'));
         Bpm.refreshTaskList();
+    },
+    'click .complete': function (evt) {
+        console.log('complete');
+        evt.preventDefault();
+        var properties = _.map($('input.formData'), function(elem){
+
+            return {
+                id: $(elem).attr("data-property-id"),
+                value: elem.value
+            }
+        });
+        //console.log("properties: %s", JSON.stringify(properties));
+        Bpm.complete(Session.get("formData").taskId, properties);
+        Bpm.refreshTaskList();
+        return false;
     }
   });
 }
 
 if (Meteor.isServer) {
     var activitiUrl = 'http://' + Bpm.activitiHost + ':8080/activiti-rest/service/';
-    var options = { auth: Bpm.auth };
+    var options = { auth: Bpm.auth, proxy: null };
 
     Meteor.startup(function () {
         Meteor.methods({
@@ -93,9 +132,44 @@ if (Meteor.isServer) {
                 options.params = {
                     taskId: taskId
                 };
-                var res = HTTP.call("GET", activitiUrl + 'form/form-data', options);
-                var content = JSON.parse(res.content);
-                return content;
+                try {
+                    var res = HTTP.call("GET", activitiUrl + 'form/form-data', options);
+                    if (res.statusCode >= 200 && res.statusCode < 300) {
+                        var result = JSON.parse(res.content);
+                        return result;
+                    } else {
+                        console.log('form/form-data returned %d', res.statusCode)
+                        return {error: 'HTTP_' + res.statusCode, taskId: taskId, formProperties: []}
+                    }
+                } catch (ex) {
+                    console.log('form/form-data returned exception %d', ex.message)
+                    return {error: ex.message, taskId: taskId, formProperties: []}
+                }
+            },
+            complete: function(taskId, properties) {
+                options.headers = {
+                    "Content-Type": "application/json"
+                };
+                options.params = {
+                    taskId: taskId
+                };
+                options.data = {
+                    taskId: parseInt(taskId),
+                    properties: properties
+                }
+                console.log(JSON.stringify(options.data));
+                try {
+                    var res = HTTP.call("POST", activitiUrl + 'form/form-data', options);
+                    if (res.statusCode >= 200 && res.statusCode < 300) {
+                        return { statusCode: res.statusCode};
+                    } else {
+                        console.log('form/form-data returned %d', res.statusCode)
+                        return { error: 'HTTP_' + res.statusCode, statusCode: res.statusCode }
+                    }
+                } catch (ex) {
+                    console.log('form/form-data returned exception %s', ex)
+                    return { error: ex.message }
+                }
             }
         });
     });
