3e7e02476feb5facaab777cec41bb1a526059184
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-05-28 23:25:26 +0200

    some cleanup

--- client/components/formWidget/formWidget.js
+++ client/components/formWidget/formWidget.js
@@ -24,12 +24,12 @@ Template.formWidget.helpers({
 
 Template.formWidget.events({
     'click .unselect': function () {
-        console.log('unselect');
+        log.info('unselect');
         Session.set('formData', undefined);
         return false;
     },
     'click .complete': function (evt) {
-        console.log('complete');
+        log.info('complete');
         evt.preventDefault();
         Bpm.complete(Session.get("formData").taskId, $('.formData'), function(err, res){
             if (err) {
@@ -48,32 +48,16 @@ Template.formWidget.events({
         return false;
     },
     'change .weird': function (evt) {
-        console.log('change weird');
+        log.info('change weird');
         var field = $(evt.target).attr('data-field');
         var ref = $(evt.target).attr('data-ref');
-        /*console.log(Session.get("formData").formProperties);*/
         var prop = _.findWhere(
                         Session.get("formData").formProperties,
                         {id: field});
-        /*console.log('data field=%s', field);
-        console.log('data ref=%s', ref);
-        console.log("prop");
-        console.log(prop);*/
         var inputHidden = $('#form_' + field);
         var data = JSON.parse(inputHidden.val());
         data[ref] = $(evt.target).val();
         inputHidden.val(JSON.stringify(data));
-        /*console.log("data after");
-        console.log(data);*/
-        //Session.get("formData").formProperties[field].data[ref] = 'ciao';
-/*
-        var container = $('form_' + $(evt.target).attr('data-field'));
-        console.log("evt");
-        console.log(evt);
-        console.log("this");
-        console.log(this);
-        console.log("container");
-        console.log(container);
-*/
+
     }
 });
