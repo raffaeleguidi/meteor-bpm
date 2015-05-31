7d751279157f0533aea92f6f1993db6667d454b2
raffaele guidi <raffaele.p.guidi@gmail.com>
2015-05-31 18:58:23 +0200

    moved scripts

--- docker/startWithoutCompose.sh
+++ docker/startWithoutCompose.sh
@@ -7,7 +7,8 @@ docker rm mysql
 docker run --name=mysql -d \
   -e 'DB_NAME=activiti_production' -e 'DB_USER=activiti' -e 'DB_PASS=password' \
     -v /opt/mysql/data:/var/lib/mysql \
-    -p 3306:3306 sameersbn/mysql:latest
+    sameersbn/mysql:latest
+    #-p 3306:3306 sameersbn/mysql:latest
 
 docker run --name=activiti -d --link mysql:mysql \
   -p 8080:8080 eternnoir/activiti:latest
